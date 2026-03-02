e0bfb3581abb3ef19daeecfd32a1674f42cd4ead
gasoonjia <gasoonjia@icloud.com>
2026-03-02 12:18:08 -0800

    enable failed tests

--- backends/test/suite/flows/cuda.py
+++ backends/test/suite/flows/cuda.py
@@ -15,48 +15,8 @@ def _create_cuda_flow(name: str = "cuda") -> TestFlow:
     The test harness serialize stage has been updated to support loading external
     data via the data_map_buffer parameter of _load_for_executorch_from_buffer.
     """
-    # Tests to skip due to known failures in CUDA backend
-    # TODO: Fix these tests and remove from skip list
-    skip_patterns = [
-        # test_mean_output_dtype: float64 output dtype causes crash in AOTI compiled code
-        "test_mean_output_dtype",
-        # Adaptive pooling ops - not fully supported
-        "test_adaptive_avgpool1d",
-        "test_adaptive_avgpool2d",
-        "test_adaptive_avgpool3d",
-        "test_adaptive_maxpool3d",
-        # 3D average pooling - not fully supported
-        "test_avgpool3d",
-        # Convolution with dilation - not fully supported
-        "test_conv1d_dilation",
-        "test_conv2d_dilation",
-        "test_conv3d_dilation",
-        # Transposed convolutions - not fully supported
-        "test_convtranspose1d",
-        "test_convtranspose2d",
-        "test_convtranspose3d",
-        # Embedding bag ops - not fully supported
-        "test_embedding_bag",
-        # Index put with multiple indices - not fully supported
-        "test_index_put_in_place_two_indices",
-        "test_index_put_two_indices",
-        # Median ops - not fully supported
-        "test_median",
-        # Select/slice/split ops - not fully supported
-        "test_select",
-        "test_slice",
-        "test_split",
-        # Threshold op - not fully supported
-        "test_threshold",
-    ]
 
-    return TestFlow(
-        name,
-        backend="cuda",
-        tester_factory=CudaTester,
-        quantize=False,
-        skip_patterns=skip_patterns,
-    )
+    return TestFlow(name, backend="cuda", tester_factory=CudaTester, quantize=False)
 
 
 CUDA_TEST_FLOW = _create_cuda_flow("cuda")
